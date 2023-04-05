9f92d31e130a12e70ecb48ff35d50d7a8f8261ba
Sagar Maheshwary <sagarmaheshwary31@gmail.com>
2023-04-05 22:36:18 +0500

    Nest Blog API: - setup sequelize postgres db connection,

--- nest-blog-api/src/app.module.ts
+++ nest-blog-api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import config from './config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   imports: [
@@ -8,6 +9,18 @@ import config from './config';
       isGlobal: true,
       load: [config],
     }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: configService.get('database.dialect'),
+        host: configService.get('database.host'),
+        database: configService.get('database.db'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        port: configService.get('database.port'),
+      })
+    })
   ],
   controllers: [],
   providers: [],
--- nest-blog-api/src/config/index.ts
+++ nest-blog-api/src/config/index.ts
@@ -7,4 +7,13 @@ export default () => ({
     url: getEnv('APP_URL'),
     env: getEnv('APP_ENV'),
   },
+  database: {
+    dialect: getEnv('DB_DIALECT'),
+    host: getEnv('DB_HOST'),
+    db: getEnv('DB_DATABASE'),
+    username: getEnv('DB_USERNAME'),
+    password: getEnv('DB_PASSWORD'),
+    port: getEnv('DB_PORT'),
+    schema: getEnv('DB_SCHEMA'),
+  }
 });
